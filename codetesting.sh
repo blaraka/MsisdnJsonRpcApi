fe31b48f27a8d306cdf1ab69579d66ed011bfb90
Mateusz Krasucki <mateusz.krasucki@gmail.com>
2015-02-05 01:57:43 +0100

    Start
    Empty files and composer settings.

---
+++ codetesting.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#Code testing script
+
+echo -e "\n1. Running php -l src/*"
+php -l src/*
+echo -e "\n2. Running php -l tests/*\n"
+php -l tests/*
+echo -e "\n3. Running phpcs --standard=PSR2\n"
+vendor/squizlabs/php_codesniffer/scripts/phpcs --standard=PSR2 src/ tests/
+echo -e "\n4. Running phpcpd --min-lines 3 --min-tokens 50\n"
+vendor/sebastian/phpcpd/phpcpd --min-lines 3 --min-tokens 50 src/ tests/
+echo -e "\n\n5. Running nphpmd src/ text codesize,design,naming,unusedcode,controversial --strict"
+vendor/phpmd/phpmd/src/bin/phpmd src/ text codesize,design,naming,unusedcode,controversial --strict
+echo -e "\n6. Running phpmd tests/ text codesize,design,naming,unusedcode,controversial --strict"
+vendor/phpmd/phpmd/src/bin/phpmd tests/ text codesize,design,naming,unusedcode,controversial --strict
+echo -e "\n"
