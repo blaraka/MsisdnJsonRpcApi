ad0f6fc3dac7e92990e05b91ad3ae1f1221c5f01
Mateusz Krasucki <mateusz.krasucki@gmail.com>
2015-02-05 15:51:31 +0100

    Added stages to puppet manifest.

--- vagrant/puppet/manifests/init.pp
+++ vagrant/puppet/manifests/init.pp
@@ -1,25 +1,32 @@
+exec { 'composer':
+    command => '/usr/local/bin composer install -d /var/www/MsisdnJsonRpcApi/';
+}
+
+include apache::mod::php
+
+stage { 'pre':
+  before => Stage["main"],
+}
+
 class { 'apache':
     default_vhost => false,
     default_mods => false,
     mpm_module => 'prefork',
+    stage => 'pre'
 }
- 
-include apache::mod::php
- 
+  
 apache::vhost { 'localhost':
     port    => '80',
     docroot => '/var/www',
-    rewrites => [ { rewrite_rule => ['^/?msisdnresolver[/]?$ /MsisdnJsonRpcApi/src/MsisdnJsonRpcApi.php'] } ]
+    rewrites => [ { rewrite_rule => ['^/?msisdnresolver[/]?$ /MsisdnJsonRpcApi/src/MsisdnJsonRpcApi.php'] } ],
+    stage = 'pre'
 }
 
 class { 'composer':
     command_name => 'composer',
     target_dir   => '/usr/local/bin',
-    auto_update => true
-}
- 
-exec { 'composer':
-    command => 'composer install -d /var/www/MsisdnJsonRpcApi/';
+    auto_update => true,
+    stage = 'pre'
 }
  
  
